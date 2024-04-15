ecd1ae96a599a46c94b1ce6a15aed7b986466caa
mmaoun137 <102172620+mmaoun137@users.noreply.github.com>
2024-04-15 13:08:17 +0300

    commit.1

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
+require('dotenv').config();  // This line should be at the top of your file
+
 const express = require('express');
 const app = express();
 const port = 3000;
+
+// Now you can use the API key securely
+console.log("TMDB API Key:", process.env.TMDB_API_KEY);
 
 app.use(express.static('../client'));
 
